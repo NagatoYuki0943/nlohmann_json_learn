e378ecc52c9d4964ecc7b68818a5933e4d01d56e
lihongtu <lihongtu@spepc.com>
2025-04-27 11:16:19 +0800

    优化代码

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
 #include <map>
@@ -8,24 +9,25 @@
 
 struct TargetState
 {
-	int id = 0;						// 靶标 id
-	bool enable = false;			// 是否启用这个靶标, 默认为 false, 用来判断这个靶标是否读取
-	double ratio = 0.0;				// 缩放比例
-	std::vector<int> box = {};		// box 位置
-	std::vector<double> standard_center = {};		// 标准靶标中心坐标
-	std::vector<double> standard_radii = {};	// 标准靶标圆环半径
-	double standard_distance = 0.0;	// 标准靶标距离
-	std::vector<double> standard_offset = {};	// 标准靶标圆环半径
-
+	bool enable = false;					  // 是否启用这个靶标, 默认为 false, 用来判断这个靶标是否读取
+	double ratio = 0.0;						  // 缩放比例
+	std::vector<int> box = {};				  // box 位置
+	std::vector<double> standard_center = {}; // 标准靶标中心坐标
+	std::vector<double> standard_radii = {};  // 标准靶标圆环半径
+	double standard_distance = 0.0;			  // 标准靶标距离
+	std::vector<double> standard_offset = {}; // 标准靶标圆环半径
 };
 
 std::map<int, TargetState> id2target_states;
 
-void save_main_data_to_json(const std::string filename = "main_data.json") {
-	try {
+void save_main_data_to_json(const std::string filename = "main_data.json")
+{
+	try
+	{
 		nlohmann::json j;
 
-		for (auto& [id, target_state] : id2target_states) {
+		for (auto &[id, target_state] : id2target_states)
+		{
 			std::string str_id = std::to_string(id);
 			j[str_id]["enable"] = target_state.enable;
 			j[str_id]["ratio"] = target_state.ratio;
@@ -38,25 +40,38 @@ void save_main_data_to_json(const std::string filename = "main_data.json") {
 
 		std::ofstream ofs(filename);
 		ofs << j.dump(4);
-	} catch (const std::exception& e) {
+
+		std::stringstream ss;
+		ss << "save main data to file: " << filename;
+		std::cout << ss.str() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << e.what() << std::endl;
 	}
 }
 
-bool is_directory(const std::string& path) {
+bool is_directory(const std::string &path)
+{
 	namespace fs = std::filesystem;
 	return fs::exists(path) && fs::is_directory(path);
 }
 
-bool is_file(const std::string& path) {
+bool is_file(const std::string &path)
+{
 	namespace fs = std::filesystem;
 	return fs::exists(path) && fs::is_regular_file(path);
 }
 
-void load_main_data_from_json(const std::string filename = "main_data.json") {
-	try {
-		if (!is_file(filename)) {
-			std::cout << "file not exist: " << filename << std::endl;
+void load_main_data_from_json(const std::string filename = "main_data.json")
+{
+	try
+	{
+		if (!is_file(filename))
+		{
+			std::stringstream ss;
+			ss << "file not exist: " << filename << ", can not load main data";
+			std::cout << ss.str() << std::endl;
 			return;
 		}
 
@@ -64,7 +79,10 @@ void load_main_data_from_json(const std::string filename = "main_data.json") {
 		std::ifstream ifs(filename);
 		ifs >> j;
 
-		for (auto& [key, value] : j.items()) {
+		id2target_states.clear();
+
+		for (auto &[key, value] : j.items())
+		{
 			bool enable = value["enable"].get<bool>();
 			if (!enable)
 				continue;
@@ -72,7 +90,6 @@ void load_main_data_from_json(const std::string filename = "main_data.json") {
 			int id = std::stoi(key);
 
 			TargetState target_state;
-			target_state.id = id;
 			target_state.enable = enable;
 			target_state.ratio = value["ratio"].get<double>();
 			target_state.box = value["box"].get<std::vector<int>>();
@@ -83,52 +100,68 @@ void load_main_data_from_json(const std::string filename = "main_data.json") {
 
 			id2target_states[id] = target_state;
 		}
+
+		std::stringstream ss;
+		ss << "load main data from file: " << filename;
+		std::cout << ss.str() << std::endl;
 	}
-	catch (const std::exception& e) {
+	catch (const std::exception &e)
+	{
 		std::cerr << e.what() << std::endl;
 	}
 }
 
-int main() {
-	id2target_states[0] = TargetState{ 0, true, 1.0, {100, 100, 200, 200}, {100.0, 100.0}, {50.0, 50.0}, 100.0, {1.0, 1.0} };
-	id2target_states[2] = TargetState{ 2, true, 2.0, {200, 200, 300, 300}, {200.0, 200.0}, {150.0, 150.0}, 200.0, {2.0, 2.0} };
-	id2target_states[3] = TargetState{ 3, false, 3.0, {300, 300, 400, 400}, {300.0, 300.0}, {250.0, 250.0}, 300.0, {3.0, 3.0} };
-	id2target_states[4] = TargetState{ 4, true, 4.0, {400, 400, 500, 500}, {400.0, 400.0}, {350.0, 350.0}, 400.0, {4.0, 4.0} };
-
-	save_main_data_to_json();
-
-	id2target_states = {};
-	std::cout << "id2target_states size: " << id2target_states.size() << std::endl;
-
-	load_main_data_from_json();
-	std::cout << "id2target_states size: " << id2target_states.size() << std::endl;
-
-	for (auto& [id, target_state] : id2target_states) {
+void print_main_data()
+{
+	for (auto &[id, target_state] : id2target_states)
+	{
 		std::cout << "id: " << id << std::endl;
 		std::cout << "enable: " << target_state.enable << std::endl;
 		std::cout << "ratio: " << target_state.ratio << std::endl;
 		std::cout << "box: [ ";
-		for (int i : target_state.box) {
+		for (int i : target_state.box)
+		{
 			std::cout << i << ", ";
 		}
 		std::cout << "]" << std::endl;
 		std::cout << "standard_center: [ ";
-		for (double i : target_state.standard_center) {
+		for (double i : target_state.standard_center)
+		{
 			std::cout << i << ", ";
 		}
 		std::cout << "]" << std::endl;
 		std::cout << "standard_radii: [ ";
-		for (double i : target_state.standard_radii) {
+		for (double i : target_state.standard_radii)
+		{
 			std::cout << i << ", ";
 		}
 		std::cout << "]" << std::endl;
 		std::cout << "standard_distance: " << target_state.standard_distance << std::endl;
 		std::cout << "standard_offset: [ ";
-		for (double i : target_state.standard_offset) {
+		for (double i : target_state.standard_offset)
+		{
 			std::cout << i << ", ";
 		}
 		std::cout << "]" << std::endl;
 	}
+}
+
+int main()
+{
+	id2target_states[0] = TargetState{true, 1.0, {100, 100, 200, 200}, {100.0, 100.0}, {50.0, 50.0}, 100.0, {1.0, 1.0}};
+	id2target_states[2] = TargetState{true, 2.0, {200, 200, 300, 300}, {200.0, 200.0}, {150.0, 150.0}, 200.0, {2.0, 2.0}};
+	id2target_states[3] = TargetState{false, 3.0, {300, 300, 400, 400}, {300.0, 300.0}, {250.0, 250.0}, 300.0, {3.0, 3.0}};
+	id2target_states[4] = TargetState{true, 4.0, {400, 400, 500, 500}, {400.0, 400.0}, {350.0, 350.0}, 400.0, {4.0, 4.0}};
+	print_main_data();
+	save_main_data_to_json();
+	std::cout << "id2target_states size: " << id2target_states.size() << std::endl;
+	std::cout << std::endl
+			  << std::endl
+			  << std::endl;
+
+	load_main_data_from_json();
+	std::cout << "id2target_states size: " << id2target_states.size() << std::endl;
+	print_main_data();
 
 	return 0;
 }
