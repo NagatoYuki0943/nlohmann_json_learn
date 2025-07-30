9521561d779cca9fa11b376f91cd504617c3028e
lihongtu <lihongtu@spepc.com>
2025-07-30 14:24:21 +0800

    refactor: 重构数据解析和序列化功能
    移除不必要的文件系统检查函数 `is_directory` 和 `is_file`。
    
    移除 `TargetState` 结构体中的 `enable` 成员变量，因为代码中并未实际使用该变量。
    
    简化 `main` 函数中的数据初始化和打印逻辑，通过调用新的 `parse_vector` 和 `parse_map` 函数来处理 `std::vector<TargetState>` 和 `std::map<int, TargetState>` 的序列化与反序列化。
    
    在 `parse_vector` 函数中实现 `std::vector<TargetState>` 的序列化与反序列化，并打印相关信息。
    
    在 `parse_map` 函数中实现 `std::map<int, TargetState>` 的序列化与反序列化，并打印相关信息。
    
    移除 `get_json_string` 函数，将其功能整合到 `parse_vector` 和 `parse_map` 函数中。
    
    移除 `parse_json_string` 函数，将其功能整合到 `parse_map` 函数中。
    
    移除 `save_main_data_to_json` 和 `load_main_data_from_json` 函数，因为它们的功能已经被 `parse_vector` 和 `parse_map` 函数所覆盖。
    
    移除 `print_main_data` 函数，因为其功能已经被新的打印逻辑所取代。

--- main.cpp
+++ main.cpp
@@ -3,296 +3,159 @@
 #include <fstream>
 #include <vector>
 #include <map>
-#include <filesystem>
 #include <nlohmann/json.hpp>
 #include "main.h"
 
 struct TargetState
 {
-	bool enable = false;					  // 是否启用这个靶标, 默认为 false, 用来判断这个靶标是否读取
 	double ratio = 0.0;						  // 缩放比例
 	std::vector<int> box = {};				  // box 位置
 	std::vector<double> standard_center = {}; // 标准靶标中心坐标
-	std::vector<double> standard_radii = {};  // 标准靶标圆环半径
-	double standard_distance = 0.0;			  // 标准靶标距离
-	std::vector<double> standard_offset = {}; // 标准靶标圆环半径
 };
 
-std::map<int, TargetState> id2target_states;
-
-bool is_directory(const std::string &path)
+void parse_vector(const int indent = 4)
 {
-	namespace fs = std::filesystem;
-	return fs::exists(path) && fs::is_directory(path);
+	std::cout << "==================== parse_vector ====================" << std::endl;
+
+	std::vector<TargetState> target_states = {
+		TargetState{1.0, {100, 100, 200, 200}, {100.0, 100.0}},
+		TargetState{2.0, {200, 200, 300, 300}, {200.0, 200.0}},
+		TargetState{3.0, {300, 300, 400, 400}, {300.0, 300.0}},
+		TargetState{4.0, {400, 400, 500, 500}, {400.0, 400.0}}};
+
+	auto print_target_states = [&target_states]() -> void
+	{
+		std::cout << "****************************************" << std::endl;
+		for (const auto target_state : target_states)
+		{
+			std::cout << "ratio: " << target_state.ratio << "; ";
+			std::cout << "box: [ ";
+			for (int i : target_state.box)
+			{
+				std::cout << i << ", ";
+			}
+			std::cout << "]" << "; ";
+			std::cout << "standard_center: [ ";
+			for (double i : target_state.standard_center)
+			{
+				std::cout << i << ", ";
+			}
+			std::cout << "]" << std::endl;
+		}
+		std::cout << "****************************************" << std::endl
+				  << std::endl;
+	};
+	print_target_states();
+
+	// -------------------- json 序列化 -------------------- //
+	nlohmann::json j;
+	for (const auto target_state : target_states)
+	{
+		nlohmann::json j1;
+		j1["ratio"] = target_state.ratio;
+		j1["box"] = target_state.box;
+		j1["standard_center"] = target_state.standard_center;
+		// 使用 push_back 方式添加到 json 数组中
+		j.push_back(j1);
+	}
+
+	std::string json_str = j.dump(indent);
+	std::cout << json_str << std::endl;
+	// -------------------- json 序列化 -------------------- //
+
+	// -------------------- json 反序列化 -------------------- //
+	nlohmann::json j1 = nlohmann::json::parse(json_str);
+
+	target_states.clear();
+	for (auto value : j1)
+	{
+		TargetState target_state;
+		target_state.ratio = value["ratio"].get<double>();
+		target_state.box = value["box"].get<std::vector<int>>();
+		target_state.standard_center = value["standard_center"].get<std::vector<double>>();
+
+		target_states.push_back(target_state);
+	}
+	print_target_states();
+	// -------------------- json 反序列化 -------------------- //
+	std::cout << "==================== parse_vector ====================" << std::endl
+			  << std::endl;
 }
 
-bool is_file(const std::string &path)
+void parse_map(const int indent = 4)
 {
-	namespace fs = std::filesystem;
-	return fs::exists(path) && fs::is_regular_file(path);
-}
+	std::cout << "==================== parse_map ====================" << std::endl;
+	std::map<int, TargetState> id2target_states;
+	id2target_states[0] = TargetState{1.0, {100, 100, 200, 200}, {100.0, 100.0}};
+	id2target_states[2] = TargetState{2.0, {200, 200, 300, 300}, {200.0, 200.0}};
+	id2target_states[3] = TargetState{3.0, {300, 300, 400, 400}, {300.0, 300.0}};
+	id2target_states[4] = TargetState{4.0, {400, 400, 500, 500}, {400.0, 400.0}};
 
-/**
-* @brief 获取 json 字符串
-*
-* @param indent 缩进, -1 表示不缩进,一行输出
-*
-* @return json 字符串
-*/
-std::string get_json_string(const int indent = 4)
-{
-	try
+	auto print_id2target_states = [&id2target_states]() -> void
 	{
-		nlohmann::json j;
-
-		for (auto &[id, target_state] : id2target_states)
+		std::cout << "****************************************" << std::endl;
+		for (const auto &[id, target_state] : id2target_states)
 		{
-			std::string str_id = std::to_string(id);
-			nlohmann::json j1;
-			j1["enable"] = target_state.enable;
-			j1["ratio"] = target_state.ratio;
-			j1["box"] = target_state.box;
-			j1["standard_center"] = target_state.standard_center;
-			j1["standard_radii"] = target_state.standard_radii;
-			j1["standard_distance"] = target_state.standard_distance;
-			j1["standard_offset"] = target_state.standard_offset;
-			j[str_id] = j1;
+			std::cout << "id: " << id << "; ";
+			std::cout << "ratio: " << target_state.ratio << "; ";
+			std::cout << "box: [ ";
+			for (int i : target_state.box)
+			{
+				std::cout << i << ", ";
+			}
+			std::cout << "]" << "; ";
+			std::cout << "standard_center: [ ";
+			for (double i : target_state.standard_center)
+			{
+				std::cout << i << ", ";
+			}
+			std::cout << "]" << std::endl;
 		}
+		std::cout << "****************************************" << std::endl
+				  << std::endl;
+	};
+	print_id2target_states();
 
-		return j.dump(indent);
-
-	}
-	catch (const std::exception &e)
+	// -------------------- json 序列化 -------------------- //
+	nlohmann::json j;
+	for (const auto &[id, target_state] : id2target_states)
 	{
-		std::stringstream ss;
-		ss << e.what();
-		std::string print_str = ss.str();
-		std::cerr << print_str << std::endl;
-		return "";
+		std::string str_id = std::to_string(id);
+		nlohmann::json j1;
+		j1["ratio"] = target_state.ratio;
+		j1["box"] = target_state.box;
+		j1["standard_center"] = target_state.standard_center;
+		j[str_id] = j1;
 	}
-}
 
-/**
-* @brief 解析 json 字符串
-*
-* @param json_str json 字符串
-*
-* @return 无
-*/
-void parse_json_string(const std::string &json_str)
-{
-	try{
-		nlohmann::json j = nlohmann::json::parse(json_str);
+	std::string json_str = j.dump(indent);
+	std::cout << json_str << std::endl;
+	// -------------------- json 序列化 -------------------- //
 
-		id2target_states.clear();
+	// -------------------- json 反序列化 -------------------- //
+	nlohmann::json j1 = nlohmann::json::parse(json_str);
 
-		for (auto &[key, value] : j.items())
-		{
-			bool enable = value["enable"].get<bool>();
-			if (!enable)
-				continue;
-
-			int id = std::stoi(key);
-
-			TargetState target_state;
-			target_state.enable = enable;
-			target_state.ratio = value["ratio"].get<double>();
-			target_state.box = value["box"].get<std::vector<int>>();
-			target_state.standard_center = value["standard_center"].get<std::vector<double>>();
-			target_state.standard_radii = value["standard_radii"].get<std::vector<double>>();
-			target_state.standard_distance = value["standard_distance"].get<double>();
-			target_state.standard_offset = value["standard_offset"].get<std::vector<double>>();
-
-			id2target_states[id] = target_state;
-		}
-
-		std::stringstream ss;
-		ss << "load main data from string successfully";
-		std::string print_str = ss.str();
-		std::cout << print_str << std::endl;
-	}
-	catch (const std::exception &e)
+	id2target_states.clear();
+	for (const auto &[key, value] : j1.items())
 	{
-		std::stringstream ss;
-		ss << e.what();
-		std::string print_str = ss.str();
-		std::cerr << print_str << std::endl;
+		int id = std::stoi(key);
+
+		TargetState target_state;
+		target_state.ratio = value["ratio"].get<double>();
+		target_state.box = value["box"].get<std::vector<int>>();
+		target_state.standard_center = value["standard_center"].get<std::vector<double>>();
+
+		id2target_states[id] = target_state;
 	}
-}
-
-/**
-* @brief 保存 main 数据到 json 文件
-*
-* @param filename 文件名
-* @param indent 缩进, -1 表示不缩进,一行输出
-*
-* @return 无
-*/
-void save_main_data_to_json(const std::string &filename = "main_data.json", const int indent = 4)
-{
-	try
-	{
-		nlohmann::json j;
-
-		for (auto &[id, target_state] : id2target_states)
-		{
-			std::string str_id = std::to_string(id);
-			nlohmann::json j1;
-			j1["enable"] = target_state.enable;
-			j1["ratio"] = target_state.ratio;
-			j1["box"] = target_state.box;
-			j1["standard_center"] = target_state.standard_center;
-			j1["standard_radii"] = target_state.standard_radii;
-			j1["standard_distance"] = target_state.standard_distance;
-			j1["standard_offset"] = target_state.standard_offset;
-			j[str_id] = j1;
-		}
-
-		std::ofstream ofs(filename);
-		ofs << j.dump(indent);
-		// ofs << j;  // 这样输出就是没有 indent 的, 一行数据
-
-		std::stringstream ss;
-		ss << "save main data to " << filename << " successfully";
-		std::string print_str = ss.str();
-		std::cout << print_str << std::endl;
-	}
-	catch (const std::exception &e)
-	{
-		std::stringstream ss;
-		ss << e.what();
-		std::string print_str = ss.str();
-		std::cerr << print_str << std::endl;
-	}
-}
-
-/**
-* @brief 从 json 文件加载 main 数据
-*
-* @param filename 文件名
-*
-* @return 无
-*/
-void load_main_data_from_json(const std::string &filename = "main_data.json")
-{
-	try
-	{
-		if (!is_file(filename))
-		{
-			std::stringstream ss;
-			ss << "file not exist: " << filename << ", load main data failed";
-			std::string print_str = ss.str();
-			std::cout << print_str << std::endl;
-			return;
-		}
-
-		nlohmann::json j;
-		std::ifstream ifs(filename);
-		ifs >> j;
-
-		// 可以直接通过 [key] 取值
-		auto j_0 = j["0"];
-		std::cout << "j_0[\"enable\"] = " << j_0["enable"] << std::endl;
-
-		id2target_states.clear();
-
-		for (auto &[key, value] : j.items())
-		{
-			bool enable = value["enable"].get<bool>();
-			if (!enable)
-				continue;
-
-			int id = std::stoi(key);
-
-			TargetState target_state;
-			target_state.enable = enable;
-			target_state.ratio = value["ratio"].get<double>();
-			target_state.box = value["box"].get<std::vector<int>>();
-			target_state.standard_center = value["standard_center"].get<std::vector<double>>();
-			target_state.standard_radii = value["standard_radii"].get<std::vector<double>>();
-			target_state.standard_distance = value["standard_distance"].get<double>();
-			target_state.standard_offset = value["standard_offset"].get<std::vector<double>>();
-
-			id2target_states[id] = target_state;
-		}
-
-		std::stringstream ss;
-		ss << "load main data from " << filename << " successfully";
-		std::string print_str = ss.str();
-		std::cout << print_str << std::endl;
-	}
-	catch (const std::exception &e)
-	{
-		std::stringstream ss;
-		ss << e.what();
-		std::string print_str = ss.str();
-		std::cerr << print_str << std::endl;
-	}
-}
-
-void print_main_data()
-{
-	for (auto &[id, target_state] : id2target_states)
-	{
-		std::cout << "id: " << id << std::endl;
-		std::cout << "enable: " << target_state.enable << std::endl;
-		std::cout << "ratio: " << target_state.ratio << std::endl;
-		std::cout << "box: [ ";
-		for (int i : target_state.box)
-		{
-			std::cout << i << ", ";
-		}
-		std::cout << "]" << std::endl;
-		std::cout << "standard_center: [ ";
-		for (double i : target_state.standard_center)
-		{
-			std::cout << i << ", ";
-		}
-		std::cout << "]" << std::endl;
-		std::cout << "standard_radii: [ ";
-		for (double i : target_state.standard_radii)
-		{
-			std::cout << i << ", ";
-		}
-		std::cout << "]" << std::endl;
-		std::cout << "standard_distance: " << target_state.standard_distance << std::endl;
-		std::cout << "standard_offset: [ ";
-		for (double i : target_state.standard_offset)
-		{
-			std::cout << i << ", ";
-		}
-		std::cout << "]" << std::endl;
-	}
+	print_id2target_states();
+	// -------------------- json 反序列化 -------------------- //
+	std::cout << "==================== parse_map ====================" << std::endl
+			  << std::endl;
 }
 
 int main()
 {
-	id2target_states[0] = TargetState{true, 1.0, {100, 100, 200, 200}, {100.0, 100.0}, {50.0, 50.0}, 100.0, {1.0, 1.0}};
-	id2target_states[2] = TargetState{true, 2.0, {200, 200, 300, 300}, {200.0, 200.0}, {150.0, 150.0}, 200.0, {2.0, 2.0}};
-	id2target_states[3] = TargetState{false, 3.0, {300, 300, 400, 400}, {300.0, 300.0}, {250.0, 250.0}, 300.0, {3.0, 3.0}};
-	id2target_states[4] = TargetState{true, 4.0, {400, 400, 500, 500}, {400.0, 400.0}, {350.0, 350.0}, 400.0, {4.0, 4.0}};
-	print_main_data();
-	save_main_data_to_json();
-	std::cout << "id2target_states size: " << id2target_states.size() << std::endl;
-	std::cout << std::endl
-			  << std::endl
-			  << std::endl;
-
-	load_main_data_from_json();
-	std::cout << "id2target_states size: " << id2target_states.size() << std::endl;
-	print_main_data();
-	std::cout << std::endl
-			  << std::endl
-			  << std::endl;
-
-	std::string json_str = get_json_string(4);
-	std::cout << "json_str:\n" << json_str << std::endl;
-	std::cout << std::endl
-			  << std::endl
-			  << std::endl;
-
-	parse_json_string(json_str);
-	print_main_data();
-
+	parse_vector();
+	parse_map();
 	return 0;
 }
